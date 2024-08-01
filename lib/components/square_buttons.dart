3b06d21bbcd77969b1a869da073ee00360fa90bc
TudorAnitas <anitas_tudor@yahoo.com>
2024-08-01 13:51:35 +0300

    add icons for the page buttons

--- lib/components/square_buttons.dart
+++ lib/components/square_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:space_traders/app_colors.dart';
 import 'package:space_traders/components/sizes.dart';
 
@@ -8,13 +9,15 @@ class SquareButton extends StatelessWidget {
   final double? height;
   final double? width;
   final EdgeInsets? margin;
+  final IconData? icon;
   const SquareButton(
       {super.key,
       required this.onPressed,
       this.text,
       this.height,
       this.width,
-      this.margin});
+      this.margin,
+      this.icon});
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +28,31 @@ class SquareButton extends StatelessWidget {
       child: FilledButton(
         onPressed: onPressed,
         style: FilledButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: icon != null
+                ? EdgeInsets.zero
+                : Theme.of(context).buttonTheme.padding),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (icon != null)
+                PhosphorIcon(
+                  icon!,
+                  size: 20,
+                ),
+              if (text != null)
+                Text(
+                  text!,
+                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: AppColors.cardBackground,
+                      ),
+                ),
+            ],
           ),
-        ),
-        child: Text(
-          text ?? '',
-          style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                color: AppColors.cardBackground,
-              ),
         ),
       ),
     );
--- lib/pages/ships/ship_details/page_buttons.dart
+++ lib/pages/ships/ship_details/page_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:space_traders/components/sizes.dart';
 import 'package:space_traders/components/square_buttons.dart';
 import 'package:space_traders/methods/duration.dart';
@@ -18,95 +19,72 @@ class ShipDetailsPageButtons extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 0,
+          children: List.generate(
+            5,
+            (index) => SquareButton(
+              onPressed: () => pageController.animateTo(pageOffset * index,
                   duration: duration, curve: curve),
-              text: '',
               height: 40,
               width: 40,
               margin: buttonMagin,
+              icon: getIcon(index),
             ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 1,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 2,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 3,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 4,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-          ],
+          ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 5,
-                  duration: duration, curve: curve),
-              text: '',
+          children: List.generate(
+            5,
+            (index) => SquareButton(
+              onPressed: () => pageController.animateTo(
+                  pageOffset * (index + 5),
+                  duration: duration,
+                  curve: curve),
               height: 40,
               width: 40,
               margin: buttonMagin,
+              icon: getIcon(index + 5),
             ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 6,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 7,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 8,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-            SquareButton(
-              onPressed: () => pageController.animateTo(pageOffset * 9,
-                  duration: duration, curve: curve),
-              text: '',
-              height: 40,
-              width: 40,
-              margin: buttonMagin,
-            ),
-          ],
+          ),
         )
       ],
     );
+  }
+
+  IconData getIcon(int index) {
+    switch (index) {
+      case 0:
+        return PhosphorIcons.mapTrifold();
+
+      case 1:
+        return PhosphorIcons.usersThree();
+
+      case 2:
+        return PhosphorIcons.airplaneTilt();
+
+      case 3:
+        return PhosphorIcons.nuclearPlant();
+
+      case 4:
+        return PhosphorIcons.engine();
+
+      case 5:
+        return PhosphorIcons.hourglass();
+
+      case 6:
+        return PhosphorIcons.lego();
+
+      case 7:
+        return PhosphorIcons.cpu();
+
+      case 8:
+        return PhosphorIcons.cube();
+
+      case 9:
+        return PhosphorIcons.gasCan();
+
+      default:
+        return PhosphorIcons.x();
+    }
   }
 }
